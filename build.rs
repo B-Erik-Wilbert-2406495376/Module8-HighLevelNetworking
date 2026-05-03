9f8639625dd3b2fae94cf8be353ff7dc1c8f035f
erikwilbert <erik.wilbert@ui.ac.id>
2026-05-03 12:45:57 +0700

    Payment Service Implementation

--- build.rs
+++ build.rs
@@ -1,9 +1,16 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let protoc_path = protoc_bin_vendored::protoc_bin_path()?;
+    
+    unsafe {
+        std::env::set_var("PROTOC", protoc_path);
+    }
+
     tonic_build::configure()
         .build_server(true)
         .compile(
             &["proto/services.proto"],
             &["proto"],
         )?;
+
     Ok(())
 }
